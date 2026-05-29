9ceaa50f6cdbd7d75dce786c57ad944affd74b62
Nino <nabatidaun@gmail.com>
2026-05-29 10:19:59 +0700

    fix: missing closing div in JobSeekerDashboard

--- src/pages/JobSeekerDashboard.jsx
+++ src/pages/JobSeekerDashboard.jsx
@@ -395,6 +395,7 @@ const JobSeekerDashboard = () => {
             </div>
 
           </div>
+        </div>
         )}
         {activeView === 'matches' && (
           <div className="p-8 max-w-7xl mx-auto space-y-8 animate-in fade-in slide-in-from-bottom-4 duration-500">
